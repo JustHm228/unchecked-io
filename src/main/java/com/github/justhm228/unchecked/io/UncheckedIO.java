41210715cf08feecae084f3a18f7a02a94661f8b
JustHuman228 <justhuman228.github@gmail.com>
2025-07-23 18:33:29 +0300

    Document ``UncheckedIO.java``
    Write javadocs for ``UncheckedIO`` utility class
    and its methods.

--- src/main/java/com/github/justhm228/unchecked/io/UncheckedIO.java
+++ src/main/java/com/github/justhm228/unchecked/io/UncheckedIO.java
@@ -30,32 +30,94 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Static utility methods to perform unchecked I/O operations.
+ *
+ * @author JustHuman228
+ * @see IORunnable
+ * @see IOSupplier
+ * @see #uncheckedIO(IORunnable)
+ * @see #uncheckedIO(IOSupplier)
+ */
 public final class UncheckedIO {
 
+    /**
+     * Prevents initialization.
+     *
+     * @throws UnsupportedOperationException Always.
+     */
     private UncheckedIO() throws UnsupportedOperationException {
 
         super();
         throw new UnsupportedOperationException("No instances for you!");
     }
 
+    /**
+     * Runs the provided function and handles thrown {@link IOException} with the provided exception handler.
+     *
+     * @param function An {@link IORunnable} to run.
+     * @param exceptionHandler An exception handler.
+     * @throws NullPointerException If any of the provided arguments is <code>null</code>.
+     * @see UncheckedIO
+     * @see #uncheckedIO(IORunnable)
+     * @see IORunnable
+     * @see IORunnable#asRunnable(Consumer)
+     */
     public static void uncheckedIO(final IORunnable function, final Consumer<? super IOException> exceptionHandler) throws NullPointerException {
 
         requireNonNull(function);
         function.asRunnable(exceptionHandler).run();
     }
 
+    /**
+     * Runs the provided function and wraps thrown {@link IOException} to {@link UncheckedIOException}.
+     *
+     * @param function An {@link IORunnable} to run.
+     * @throws NullPointerException If the provided function is <code>null</code>.
+     * @throws UncheckedIOException If the provided function throws an {@link IOException}.
+     * @see UncheckedIO
+     * @see #uncheckedIO(IORunnable, Consumer)
+     * @see IORunnable
+     * @see IORunnable#asRunnable()
+     */
     public static void uncheckedIO(final IORunnable function) throws NullPointerException, UncheckedIOException {
 
         requireNonNull(function);
         function.asRunnable().run();
     }
 
+    /**
+     * Runs the provided function and returns the result, while handling thrown {@link IOException} with the provided exception handler.
+     *
+     * @param function An {@link IOSupplier} to run.
+     * @param exceptionHandler An exception handler.
+     * @return The result.
+     * @param <T> The type of result.
+     * @throws NullPointerException If any of the provided arguments is <code>null</code>.
+     * @see UncheckedIO
+     * @see #uncheckedIO(IOSupplier)
+     * @see IOSupplier
+     * @see IOSupplier#asSupplier(Function)
+     */
     public static <T> T uncheckedIO(final IOSupplier<T> function, final Function<? super IOException, ? extends T> exceptionHandler) throws NullPointerException {
 
         requireNonNull(function);
         return function.asSupplier(exceptionHandler).get();
     }
 
+    /**
+     * Runs the provided function and returns the result, while wrapping thrown {@link IOException} to {@link UncheckedIOException}.
+     *
+     * @param function An {@link IOSupplier} to run.
+     * @return The result.
+     * @param <T> The type of result.
+     * @throws NullPointerException If the provided function is <code>null</code>.
+     * @throws UncheckedIOException If the provided function throws an {@link IOException}.
+     * @see UncheckedIO
+     * @see #uncheckedIO(IOSupplier, Function)
+     * @see IOSupplier
+     * @see IOSupplier#asSupplier()
+     */
     public static <T> T uncheckedIO(final IOSupplier<T> function) throws NullPointerException, UncheckedIOException {
 
         requireNonNull(function);
